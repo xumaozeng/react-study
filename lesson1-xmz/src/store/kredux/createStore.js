403ffb26a936fde7a8188a0e45fe3bd83f1d67db
xumaozeng <15611895341@163.com>
2021-01-29 10:22:48 +0800

    state初始值解决

--- lesson1-xmz/src/store/kredux/createStore.js
+++ lesson1-xmz/src/store/kredux/createStore.js
@@ -1,5 +1,5 @@
 export default function createStore(reducer) {
-  let currentState = null;
+  let currentState; // 初始为null 默认值没有作用(state为undefined走default,内部采用严格相等模式)
   let currentListeners = [];
 
   function getState() {
@@ -12,6 +12,9 @@ export default function createStore(reducer) {
   function subscribe(listener) {
     currentListeners.push(listener);
   }
+
+  // state默认值
+  dispatch({ type: "REDUXXXXXXX" });
   return {
     getState,
     dispatch,
