aefc1516789eebe199422e0c035484591bf20c6c
xumaozeng <15611895341@163.com>
2021-01-29 18:00:51 +0800

    redux-combineReducersAPI实现

--- lesson1-xmz/src/store/kredux/combineReducers.js
+++ lesson1-xmz/src/store/kredux/combineReducers.js
@@ -1,1 +1,17 @@
-export default function combineReducers() {}
+export default function combineReducers(reducers) {
+  return function combination(state = {}, action) {
+    let nextState = {};
+    let hasChanged = false; // 初始值
+    for (let key in reducers) {
+      const reducer = reducers[key];
+      const previousStateForKey = state[key];
+      const nextStateForKey = reducer(previousStateForKey, action);
+      nextState[key] = nextStateForKey;
+      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
+    }
+
+    hasChanged =
+      hasChanged || Object.keys(nextState).length !== Object.keys(state).length;
+    return hasChanged ? nextState : state;
+  };
+}
