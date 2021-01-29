8da365287092fcc239b2c8aa439ee51005456e0f
xumaozeng <15611895341@163.com>
2021-01-29 10:29:22 +0800

    subscribe

--- lesson1-xmz/src/pages/ReduxPage.js
+++ lesson1-xmz/src/pages/ReduxPage.js
@@ -3,9 +3,17 @@ import store from "../store";
 
 class ReduxPage extends Component {
   componentDidMount() {
-    store.subscribe(() => {
+    // store发生变化之后，执行subscribe的监听函数
+    this.unsubscribe = store.subscribe(() => {
       this.forceUpdate();
     });
+  }
+
+  componentWillUnmount() {
+    // 组件卸载之前，取消订阅
+    if (this.unsubscribe) {
+      this.unsubscribe();
+    }
   }
   add = () => {
     store.dispatch({ type: "ADD", payload: 100 });
--- lesson1-xmz/src/store/kredux/createStore.js
+++ lesson1-xmz/src/store/kredux/createStore.js
@@ -2,15 +2,25 @@ export default function createStore(reducer) {
   let currentState; // 初始为null 默认值没有作用(state为undefined走default,内部采用严格相等模式)
   let currentListeners = [];
 
+  // get
   function getState() {
     return currentState;
   }
+
+  // set
   function dispatch(action) {
     currentState = reducer(currentState, action);
+    // state改变，执行订阅的函数
     currentListeners.forEach(listener => listener());
   }
+
+  // 订阅和取消订阅要成对出现
   function subscribe(listener) {
     currentListeners.push(listener);
+    return () => {
+      const index = currentListeners.indexOf(listener);
+      currentListeners.splice(index, 1);
+    };
   }
 
   // state默认值
