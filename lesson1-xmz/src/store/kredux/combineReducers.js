ed3f8f69d7a980681c7e23580bfbd45e9b20aa9b
xumaozeng <15611895341@163.com>
2021-01-30 18:36:02 +0800

    combineReducers

--- lesson1-xmz/src/store/kredux/combineReducers.js
+++ lesson1-xmz/src/store/kredux/combineReducers.js
@@ -4,12 +4,15 @@ export default function combineReducers(reducers) {
     let hasChanged = false; // 初始值
     for (let key in reducers) {
       const reducer = reducers[key];
-      const previousStateForKey = state[key];
-      const nextStateForKey = reducer(previousStateForKey, action);
-      nextState[key] = nextStateForKey;
-      hasChanged = hasChanged || nextStateForKey !== previousStateForKey;
+      nextState[key] = reducer(state[key], action);
+      hasChanged = hasChanged || nextState[key] !== state[key];
     }
 
+    /**
+     * 动态改变reducer
+     * {a: 1, b:2}
+     * {a: 1 }
+     */
     hasChanged =
       hasChanged || Object.keys(nextState).length !== Object.keys(state).length;
     return hasChanged ? nextState : state;
