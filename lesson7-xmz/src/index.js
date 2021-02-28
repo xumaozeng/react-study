5f5cef99b24aede0969ce5658e2aa8c7f2ef67dd
xumaozeng <15611895341@163.com>
2021-02-28 16:33:52 +0800

    lesson7-xmz

---
+++ lesson7-xmz/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const jsx = (
+  <div>
+    <h3>React原理解析-JSX</h3>
+  </div>
+);
+
+ReactDOM.render(jsx, document.getElementById("root"));
