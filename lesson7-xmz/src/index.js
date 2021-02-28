f00e74bba5963f51242882fc6fb4a78b92dc6eae
xumaozeng <15611895341@163.com>
2021-02-28 22:58:25 +0800

    React原理-虚拟dom

--- lesson7-xmz/src/index.js
+++ lesson7-xmz/src/index.js
@@ -1,10 +1,14 @@
+// import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
 const jsx = (
-  <div>
-    <h3>React原理解析-JSX</h3>
+  <div className="border">
+    <h1>React原理解析-01</h1>
+    <a href="https://www.kaikeba.com">kkb</a>
   </div>
 );
 
 ReactDOM.render(jsx, document.getElementById("root"));
+
+// console.log(React.version);
